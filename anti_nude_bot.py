648d4efa95c9dc606586564e4daaa0b995fff1af
Sam <sam-marx@protonmail.com>
2019-07-01 13:47:50 -0300

    deve estar funcionando agora

--- anti_nude_bot.py
+++ anti_nude_bot.py
@@ -12,15 +12,17 @@ import os
 import re
 import datetime
 from pytz import timezone
-import nude
 import shutil
-from nude import Nude
+from nudity import Nudity
+from PIL import Image
 
 API_TOKEN = os.getenv('TOKEN')
 
 REG_GROUP = os.getenv('REGISTER')
 
 modo = os.getenv('MODO')
+
+nudity = Nudity()
 
 logging.basicConfig(level=logging.INFO)
 
@@ -43,22 +45,25 @@ else:
 	sys.exit()
 
 @run_async
-def check_nude(bot, update):
-	foto = bot.get_file(update.message.photo[-1].file_id)
-	foto = str(foto) #transformando o tipo type para string
-	foto = foto.replace("'", '"') #substituindo os ' por "
+def check_nude_sticker(bot, update):
+	sticker = str(bot.get_file(update.message.sticker.file_id)).replace("'", '"')
 
-	js = json.loads(foto) #carregando o json
-	foto = js['file_path'] #conseguindo a fonte da imagem
+	js = json.loads(sticker)
+	sticker = js['file_path']
 
-	r = requests.get(foto, stream=True) #fazendo a requisição para baixar a img
-	foto = foto.split('/')[-1] #dividindo para pegar apenas o nome e a extensão
+	r = requests.get(sticker, stream=True)
+	sticker = sticker.split('/')[-1]
 
-	with open(foto, 'wb') as fo:
+	with open(sticker, 'wb') as fo:
 		shutil.copyfileobj(r.raw, fo)
 	del r
 
-	if nude.is_nude(str(foto)) == True: #checando se tem nude ou n com nudepy
+	#transformando de webp para jpg
+	im = Image.open(sticker).convert('RGB') 
+	sticker = sticker.split('.')[0] + '.jpg'
+	im.save(sticker, 'jpeg') #salvando sticker
+
+	if nudity.has(str(sticker)) == True: #checando se tem nude ou n com nudity
 		alvo_id = update.message.from_user.id
 		alvo_usuario = update.message.from_user.username
 		alvo_nome = update.message.from_user.first_name
@@ -79,13 +84,64 @@ def check_nude(bot, update):
 		group_name=update.message.chat.title,
 		group_id=update.message.chat.id,
 		data=datetime.datetime.now(timezone('America/Sao_Paulo')).strftime('%H:%M %d %B, %Y'),
-	  	motivo='Pornografia')
+		motivo='Pornografia')
 
 		bot.kick_chat_member(chat_id=update.message.chat_id, user_id=alvo_id)
 		bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text=banido, reply_to_message_id=update.message.message_id)
 		bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
 		bot.send_message(parse_mode='HTML', chat_id=REG_GROUP, text=banido_usuario)
 	else: pass
+
+	os.remove(str(sticker)) #remove os stickers salvos para n ocupar espaço
+	os.remove(str(sticker.split('.')[0] + '.webp')) #remove os stickers salvos para n ocupar espaço
+
+@run_async
+def check_nude_image(bot, update):
+	foto = bot.get_file(update.message.photo[-1].file_id)
+
+	foto = str(foto) #transformando o tipo type para string
+	foto = foto.replace("'", '"') #substituindo os ' por "
+
+	js = json.loads(foto) #carregando o json
+	foto = js['file_path'] #conseguindo a fonte da imagem
+
+	r = requests.get(foto, stream=True) #fazendo a requisição para baixar a img
+	foto = foto.split('/')[-1] #dividindo para pegar apenas o nome e a extensão
+
+	with open(foto, 'wb') as fo:
+		shutil.copyfileobj(r.raw, fo)
+	del r
+
+	if nudity.has(str(foto)) == True: #checando se tem nude ou n com nudepy
+		alvo_id = update.message.from_user.id
+		alvo_usuario = update.message.from_user.username
+		alvo_nome = update.message.from_user.first_name
+
+		if alvo_usuario == None:
+			banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_nome, alvo_id)
+		else:
+			banido = '<b>Usuário {} - {} banido por envio de pornografia.</b>'.format(alvo_usuario, alvo_id)
+
+		banido_usuario = '''
+#BANIDO_USUARIO
+<b>Usuário: </b>{user_name} [{user_id}]
+<b>Grupo: </b>{group_name} [{group_id}]
+<b>Data: </b>{data}
+<b>Motivo: </b>{motivo}
+'''.format(user_name=alvo_usuario,
+		user_id=alvo_id,
+		group_name=update.message.chat.title,
+		group_id=update.message.chat.id,
+		data=datetime.datetime.now(timezone('America/Sao_Paulo')).strftime('%H:%M %d %B, %Y'),
+		motivo='Pornografia')
+
+		bot.kick_chat_member(chat_id=update.message.chat_id, user_id=alvo_id)
+		bot.send_message(parse_mode='HTML', chat_id=update.message.chat_id, text=banido, reply_to_message_id=update.message.message_id)
+		bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
+		bot.send_message(parse_mode='HTML', chat_id=REG_GROUP, text=banido_usuario)
+	else: pass
+
+	os.remove(str(foto))
 
 def main():
 	updater = Updater(token=API_TOKEN)
